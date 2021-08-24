ab4eca8de7d097176181a0bd3e7e59c6fc562f93
stonehank <stonehank310@gmail.com>
2021-08-24 21:50:13 +0800

    some update

--- src/pages/index/src/components/HomepageLoading.vue
+++ src/pages/index/src/components/HomepageLoading.vue
@@ -1,5 +1,5 @@
 <template>
-    <section style="height:100vh;" class="w-100 blur-transition blur-10 accord-wrapper" ref="blur-loading" :style="{marginTop:- $state.getNavH() + 'px'}">
+    <section style="height:100vh;padding-right:17px;" class="w-100 blur-transition blur-10 accord-wrapper" ref="blur-loading" :style="{marginTop:- $state.getNavH() + 'px'}">
         <div class="skew-box color-1"></div>
         <div class="skew-box color-2"></div>
         <div class="skew-box color-3"></div>
