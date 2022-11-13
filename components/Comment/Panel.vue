500584f07e04a84ab296dfda65c8f7afa0f440e3
stonehank <stonehank310@gmail.com>
2022-11-13 20:00:54 +0800

    UPDATE: doc

--- components/Comment/Panel.vue
+++ components/Comment/Panel.vue
@@ -3,12 +3,7 @@
 </template>
 
 <script>
-// import VueBbs from "vue-bbs/dist/lib/vue-bbs";
-// import LeancloudLayer from 'vue-bbs/dist/backend/leancloud'
-// import registerServer from 'vue-bbs/dist/registerServer'
-// registerServer(VueBbs,LeancloudLayer)
 export default {
-    // components: {VueBbs},
     props: {
         uniqStr: {
             type: String,
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,80 @@
 <template>
-    <section :style="`margin-top:-${$getNavH()}px`">
-        <HomeOverview />
-        <client-only>
-            <section style="height: 100vh">
-                <v-container style="max-width: 980px">
-                    <v-row>
-                        <v-col
-                            cols="12"
-                            :style="{ marginTop: $getNavH() + 'px' }"
-                        >
-                            <h2>评论区</h2>
-                        </v-col>
-                        <v-col cols="12">
-                            <CommentPanel uniq-str="home-page" />
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </section>
-        </client-only>
+    <section
+        :style="{
+            'margin-top': `-${$getNavH()}px`,
+            height: `${$getWinH()}px`,
+            overflow: 'hidden',
+        }"
+        class="position-relative"
+    >
+        <swiper class="swiper" :options="swiperOption">
+            <swiper-slide>
+                <HomeOverview />
+            </swiper-slide>
+            <swiper-slide>
+                <section style="height: 100vh">
+                    <v-container style="max-width: 980px">
+                        <v-row>
+                            <v-col
+                                cols="12"
+                                :style="{ marginTop: $getNavH() + 'px' }"
+                            >
+                                <h2>评论区</h2>
+                            </v-col>
+                            <v-col cols="12">
+                               <client-only>
+                                 <CommentPanel uniq-str="home-page" />
+                               </client-only>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </section>
+            </swiper-slide>
+        </swiper>
     </section>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 export default {
     name: 'IndexPage',
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
     inject: ['hideNav', 'showNav'],
+    data() {
+        const self = this
+        return {
+            swiperOption: {
+                direction: 'vertical',
+                slidesPerView: 1,
+                spaceBetween: 30,
+                mousewheel: true,
+                on: {
+                    slideChangeTransitionEnd: function () {
+                        const idx = this.activeIndex
+                        if (idx === 0) {
+                            self.hideNav()
+                        } else {
+                            self.showNav()
+                        }
+                    },
+                },
+            },
+        }
+    },
     created() {
         this.hideNav()
     },
     mounted() {
-        $(window).on('scroll', this.toggleNavOnScroll)
+        // $(window).on('scroll', this.toggleNavOnScroll)
     },
     beforeDestroy() {
         this.showNav()
     },
     destroyed() {
-        $(window).off('scroll', this.toggleNavOnScroll)
+        // $(window).off('scroll', this.toggleNavOnScroll)
     },
     methods: {
         toggleNavOnScroll() {
@@ -48,3 +87,11 @@ export default {
     },
 }
 </script>
+<style lang="scss" scoped>
+::v-deep .swiper {
+    height: 100%;
+    .swiper-wrapper {
+        height: 100%;
+    }
+}
+</style>
