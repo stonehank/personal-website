73608861712323fa1c61f2689709c5da83694bb6
stonehank <stonehank310@gmail.com>
2022-11-13 21:48:14 +0800

    ADD: publicPath

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,7 @@ export default {
         anime: ['animejs', 'default'],
       })
     ],
+    publicPath:'https://stonehank.github.io'
   },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
